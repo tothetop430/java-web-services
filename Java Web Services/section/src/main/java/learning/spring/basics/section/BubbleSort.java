ad648c9bb25d5653a84261c52b43edfe8180432e
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-08 19:38:17 +0530

    create bubble sort

---
+++ Java Web Services/section/src/main/java/learning/spring/basics/section/BubbleSort.java
@@ -0,0 +1,11 @@
+package learning.spring.basics.section;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BubbleSort implements SortAlgo {
+	public int[] sort(int[] numbers) {
+		//does sorting
+		return numbers;
+	}
+}
