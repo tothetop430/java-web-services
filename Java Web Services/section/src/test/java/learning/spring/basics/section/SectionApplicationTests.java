1df1695f4fa92a6b9d6d1d427c90f4750b8ab4b1
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-08 19:38:43 +0530

    spring application tests

---
+++ Java Web Services/section/src/test/java/learning/spring/basics/section/SectionApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.basics.section;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
