8158026696818ec0d610f417bf3de2b323146a3e
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-10 22:53:58 +0530

    create UserDetails

---
+++ user-details/src/test/java/learning/rest/webservice/userdetails/UserDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.rest.webservice.userdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
