4b15824cb27ef8fee65799a89433c8a4fdb29871
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-11 22:07:37 +0530

    create a post request createUser

--- user-details/src/main/java/learning/rest/webservice/userdetails/user/UserResourceController.java
+++ user-details/src/main/java/learning/rest/webservice/userdetails/user/UserResourceController.java
@@ -1,11 +1,16 @@
 package learning.rest.webservice.userdetails.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResourceController {
@@ -21,5 +26,18 @@ public class UserResourceController {
 	@GetMapping(path="/users/{id}")
 	public User retrieveUser(@PathVariable Integer id) {
 		return userDaoService.findUser(id);
+	}
+	
+	@PostMapping(path="/users")
+	public void createUser(@RequestBody User user) {
+		User createdUser = userDaoService.saveUser(user);
+		
+//		URI location = ServletUriComponentsBuilder
+//				.fromCurrentRequest()
+//				.path("/{id}")
+//				.buildAndExpand(createdUser.getId())
+//				.toUri();
+//		
+//		return ResponseEntity.created(location).build();
 	}
 }
