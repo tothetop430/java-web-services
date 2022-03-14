acf79d020f6a2fe360a7610355d9eac67cc8462c
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-14 17:13:38 +0530

    create SwaggerDocumentation

--- user-details/src/main/java/learning/rest/webservice/userdetails/user/UserResourceController.java
+++ user-details/src/main/java/learning/rest/webservice/userdetails/user/UserResourceController.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 
 import javax.validation.Valid;
 
-//import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.EntityModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
