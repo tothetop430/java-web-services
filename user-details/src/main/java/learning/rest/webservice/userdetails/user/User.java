35a05bf2f5c318d2480b046a166fb696b09fee98
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-14 12:34:24 +0530

    add validation @Size @Past

--- user-details/src/main/java/learning/rest/webservice/userdetails/user/User.java
+++ user-details/src/main/java/learning/rest/webservice/userdetails/user/User.java
@@ -2,10 +2,17 @@ package learning.rest.webservice.userdetails.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	
 	private Integer id;
+	
+	@Size(min=2, message="Name should be atleast 2 characters long.")
 	private String name;
+	
+	@Past
 	private Date birthDate;
 	
 	public User(Integer id, String name, Date birthDate) {
