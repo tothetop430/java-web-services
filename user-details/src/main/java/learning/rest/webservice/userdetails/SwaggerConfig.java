15e768054b8c024a0c7f092e903ceb1615c46302
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-16 16:10:24 +0530

    add apiInfo to Docket

--- user-details/src/main/java/learning/rest/webservice/userdetails/SwaggerConfig.java
+++ user-details/src/main/java/learning/rest/webservice/userdetails/SwaggerConfig.java
@@ -3,6 +3,7 @@ package learning.rest.webservice.userdetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,9 +12,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	private static final ApiInfo DEFAULT_CONTACT = null;
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_CONTACT);
 	}
 	
 }
