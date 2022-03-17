567cb44a338ad22c059571d33b3082053039e7ca
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-17 12:17:28 +0530

    setup JPA project files

---
+++ Jpa-Project/src/test/java/learning/jpa/Jpa/Project/JpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.jpa.Jpa.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
