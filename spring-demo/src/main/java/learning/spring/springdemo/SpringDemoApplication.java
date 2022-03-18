dbf44f6721824c037d02fec502ecea7f54cddbbf
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-18 21:09:04 +0530

    close application context and add slf4j and spring-plugin-core

--- spring-demo/src/main/java/learning/spring/springdemo/SpringDemoApplication.java
+++ spring-demo/src/main/java/learning/spring/springdemo/SpringDemoApplication.java
@@ -2,16 +2,17 @@ package learning.spring.springdemo;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(SpringDemoApplication.class);
 		System.out.println(context.getApplicationName());
+		System.out.println(123);
+		((AbstractApplicationContext) context).close();
 	}
 
 }
