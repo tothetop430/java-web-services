5a74ab5113e2cae4d5e0cfab1ec7aacb62a1cd53
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-21 22:08:23 +0530

    learning mockito

---
+++ mockito-testing/src/main/java/learning/mockito/mockitotesting/MockitoTestingApplication.java
@@ -0,0 +1,13 @@
+package learning.mockito.mockitotesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoTestingApplication.class, args);
+	}
+
+}
---
+++ mockito-testing/src/test/java/learning/mockito/mockitotesting/MockitoTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.mockito.mockitotesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
