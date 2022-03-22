d088bbe3ab44ea1ac3d230dec8dad8c62a8b4ce6
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-22 22:57:50 +0530

    add sample data layer

---
+++ aop/src/main/java/learning/springaop/aop/data/Dao2.java
@@ -0,0 +1,11 @@
+package learning.springaop.aop.data;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao2 {
+
+	public String retrieveSomething() {
+		return "Dao2";
+	}
+}
