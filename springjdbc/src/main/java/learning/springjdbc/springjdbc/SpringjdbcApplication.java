20a324333719021c5f341d6a6d842eb834cad057
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-24 19:29:23 +0530

    add command line runner to the application

--- springjdbc/src/main/java/learning/springjdbc/springjdbc/SpringjdbcApplication.java
+++ springjdbc/src/main/java/learning/springjdbc/springjdbc/SpringjdbcApplication.java
@@ -1,13 +1,29 @@
 package learning.springjdbc.springjdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringjdbcApplication {
+import learning.springjdbc.springjdbc.jdbc.PersonJdbcDao;
 
+@SpringBootApplication
+public class SpringjdbcApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringjdbcApplication.class);
+	
+	@Autowired
+	private PersonJdbcDao personDao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringjdbcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("" + personDao.findAll());
 	}
 
 }
