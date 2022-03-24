4226658ddf51db87a0d45ab7355931312c6c9c84
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-24 17:08:51 +0530

    create spring application test

---
+++ springjdbc/src/test/java/learning/springjdbc/springjdbc/SpringjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.springjdbc.springjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
