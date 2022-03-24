6771c401e5944e4fb9b96ce3d190db15522f1197
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-24 19:29:03 +0530

    create person dao service to execute SQL queries

---
+++ springjdbc/src/main/java/learning/springjdbc/springjdbc/jdbc/PersonJdbcDao.java
@@ -0,0 +1,22 @@
+package learning.springjdbc.springjdbc.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import learning.springjdbc.springjdbc.entity.Person;
+
+@Repository
+public class PersonJdbcDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public List<Person> findAll() {
+		return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+	}
+
+}
