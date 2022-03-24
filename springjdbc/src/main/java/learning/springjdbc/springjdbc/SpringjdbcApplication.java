27f75e0ec7406bb183b4fbf5c3eefe143e8818f1
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-24 17:07:52 +0530

    add Spring application

---
+++ springjdbc/src/main/java/learning/springjdbc/springjdbc/SpringjdbcApplication.java
@@ -0,0 +1,13 @@
+package learning.springjdbc.springjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjdbcApplication.class, args);
+	}
+
+}
