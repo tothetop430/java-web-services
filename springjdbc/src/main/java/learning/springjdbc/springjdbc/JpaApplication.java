9e7099aba9a4f6c065aa0625b598f2f8bc8da0b1
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-25 22:47:13 +0530

    create JpaApplication SpringBootApplication

---
+++ springjdbc/src/main/java/learning/springjdbc/springjdbc/JpaApplication.java
@@ -0,0 +1,31 @@
+package learning.springjdbc.springjdbc;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import learning.springjdbc.springjdbc.jdbc.JpaRepositoryDao;
+
+
+@SpringBootApplication
+public class JpaApplication implements CommandLineRunner {
+	
+	private static final Logger logger = LoggerFactory.getLogger(JpaApplication.class);
+	
+	@Autowired
+	private JpaRepositoryDao jpaDao;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		logger.info("Person By Id" + jpaDao.findById(10001));
+	}
+}
