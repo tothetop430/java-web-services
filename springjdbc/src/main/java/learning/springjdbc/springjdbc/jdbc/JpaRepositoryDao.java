bed5661413d2d7145322f5061245303ed74be7d9
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-25 22:46:55 +0530

    create EntityManager to manage PersistenceContext

---
+++ springjdbc/src/main/java/learning/springjdbc/springjdbc/jdbc/JpaRepositoryDao.java
@@ -0,0 +1,24 @@
+package learning.springjdbc.springjdbc.jdbc;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import learning.springjdbc.springjdbc.entity.PersonTable;
+
+@Repository
+@Transactional
+public class JpaRepositoryDao {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public PersonTable findById(int id) {
+		return entityManager.find(PersonTable.class, id);
+	}
+	
+	
+	
+}
